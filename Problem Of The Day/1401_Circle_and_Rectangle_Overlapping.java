bcb43bc52ed98fb5af2f39fb9cb7a74aa16f8a90
madhuananthaneni123-ui <madhuananthaneni123@gmail.com>
2026-09-19 19:06:47 +0530

    Create 1401_Circle_and_Rectangle_Overlapping.java

---
+++ Problem Of The Day/1401_Circle_and_Rectangle_Overlapping.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean checkOverlap(int radius, int xc, int yc, int x1, int y1, int x2, int y2) {
+        int x=Math.max(x1,Math.min(xc,x2))-xc;
+        int y=Math.max(y1,Math.min(yc,y2))-yc;
+        return x*x+y*y<=radius*radius;
+    }
+}
