875408ac8440e15f70f04e52f656668ff672529e
madhuananthaneni123-ui <madhuananthaneni123@gmail.com>
2026-09-26 18:19:01 +0530

    Create 1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome.java

---
+++ Dynamic Programming/1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minInsertions(String s) {
+        int n=s.length();
+        int[][] dp=new int[n+1][n+1];
+        String s2=new StringBuilder(s).reverse().toString();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return n-dp[n][n];
+    }
+}
